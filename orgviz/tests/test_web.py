1286d14b59ab8ba59da6b6608e01f3d1eaa8ca9a
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-23 15:46:39 +0200

    Add TestWebEventsData.test_start_end

--- orgviz/tests/test_web.py
+++ orgviz/tests/test_web.py
@@ -4,10 +4,16 @@ import shutil
 import unittest
 import textwrap
 import json
+import datetime
 
 from .. import web
 
 TMP_PREFIX = 'orgviz-test-'
+
+
+def totimestamp(dt):
+    zero = datetime.datetime.fromtimestamp(0)
+    return (dt - zero).total_seconds()
 
 
 class TestWebEventsData(unittest.TestCase):
@@ -18,7 +24,7 @@ class TestWebEventsData(unittest.TestCase):
         cls.org_file = os.path.join(cls.tmpdir, 'test.org')
         web.app.config['ORG_FILE_COMMON'] = [cls.org_file]
         web.app.config['ORG_FILE_CAL'] = []
-        web.app.config['NO_CACHE'] = False
+        web.app.config['NO_CACHE'] = True
         cls.app = web.app.test_client()
 
     @classmethod
@@ -35,3 +41,23 @@ class TestWebEventsData(unittest.TestCase):
         events_data = json.loads(rv.data)
         self.assertEqual(len(events_data), 1)
         self.assertEqual(events_data[0]['title'], 'Node title')
+
+    def test_start_end(self):
+        with open(self.org_file, 'w') as f:
+            f.write(textwrap.dedent("""
+            * Node 1
+              SCHEDULED: <2012-10-21 Tue>
+            * Node 2
+              SCHEDULED: <2012-10-22 Wed>
+            * Node 3
+              SCHEDULED: <2012-10-24 Fri>
+            """))
+        # FIXME: clarify boundary condition 2012-10-23 in Node 3 does not work!
+        start = totimestamp(datetime.datetime(2012, 10, 20))
+        end = totimestamp(datetime.datetime(2012, 10, 23))
+        rv = self.app.get(
+            '/events_data?start={0:.0f}&end={1:.0f}'.format(start, end))
+        events_data = json.loads(rv.data)
+        self.assertEqual(len(events_data), 2)
+        self.assertEqual(events_data[0]['title'], 'Node 1')
+        self.assertEqual(events_data[1]['title'], 'Node 2')
