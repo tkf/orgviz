8441f5c2d4933bb5ac73972ca7a7369ad766e03d
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-23 15:58:13 +0200

    Refactor TestWebEventsData

--- orgviz/tests/test_web.py
+++ orgviz/tests/test_web.py
@@ -31,32 +31,37 @@ class TestWebEventsData(unittest.TestCase):
     def tearDownClass(cls):
         shutil.rmtree(cls.tmpdir)
 
-    def test_single_event(self):
+    def write_org_file(self, text):
         with open(self.org_file, 'w') as f:
-            f.write(textwrap.dedent("""
-            * Node title
-              SCHEDULED: <2012-10-23 Tue>
-            """))
+            f.write(textwrap.dedent(text))
+
+    def test_single_event(self):
+        self.write_org_file("""
+        * Node title
+          SCHEDULED: <2012-10-23 Tue>
+        """)
         rv = self.app.get('/events_data?start=1349042400&end=1352674800')
         events_data = json.loads(rv.data)
         self.assertEqual(len(events_data), 1)
         self.assertEqual(events_data[0]['title'], 'Node title')
 
-    def test_start_end(self):
-        with open(self.org_file, 'w') as f:
-            f.write(textwrap.dedent("""
-            * Node 1
-              SCHEDULED: <2012-10-21 Tue>
-            * Node 2
-              SCHEDULED: <2012-10-22 Wed>
-            * Node 3
-              SCHEDULED: <2012-10-24 Fri>
-            """))
-        # FIXME: clarify boundary condition 2012-10-23 in Node 3 does not work!
-        start = totimestamp(datetime.datetime(2012, 10, 20))
-        end = totimestamp(datetime.datetime(2012, 10, 23))
-        rv = self.app.get(
+    def get_events_data(self, start, end):
+        start = totimestamp(datetime.datetime(*start))
+        end = totimestamp(datetime.datetime(*end))
+        return self.app.get(
             '/events_data?start={0:.0f}&end={1:.0f}'.format(start, end))
+
+    def test_start_end(self):
+        self.write_org_file("""
+        * Node 1
+          SCHEDULED: <2012-10-21 Tue>
+        * Node 2
+          SCHEDULED: <2012-10-22 Wed>
+        * Node 3
+          SCHEDULED: <2012-10-24 Fri>
+        """)
+        # FIXME: clarify boundary condition 2012-10-23 in Node 3 does not work!
+        rv = self.get_events_data(start=(2012, 10, 20), end=(2012, 10, 23))
         events_data = json.loads(rv.data)
         self.assertEqual(len(events_data), 2)
         self.assertEqual(events_data[0]['title'], 'Node 1')
