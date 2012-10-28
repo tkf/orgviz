2ef51949413837ccff59cbf383ac395c3250c3d0
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-28 23:05:23 +0100

    Document configuration variables

--- orgviz/default_config.py
+++ orgviz/default_config.py
@@ -24,6 +24,20 @@ Org-mode files to include calendar view.
 """
 
 ORG_CAL_FILTERS = []
+"""
+List of (name, function)-pairs to specify events to show in calendar.
+
+The first element of the tuple must be human readable name of filter
+and the second element must be a callable object (function).  Each
+function must take one :class:`orgviz.event.Event` object and return a
+bool.
+
+The order of the filters is reflected to the order in the web
+interface.  That's why this option is specified as a list (association
+list), instead of a dict.
+
+"""
+
 # FIXME: error occurs in JS when ORG_CAL_PERSPECTIVES is []
 ORG_CAL_PERSPECTIVES = [
     ('month', {
@@ -35,9 +49,70 @@ ORG_CAL_PERSPECTIVES = [
         'view': 'month',
         }),
 ]
+"""
+Calendar perspectives definitions.
+
+To quickly switch events to include, calendar view and filters, you
+can define perspectives.  Perspective is defined in the following
+format::
+
+   [('PERSPECTIVE NAME', {
+        'event': ['deadline', 'scheduled', ...],  # closed/clock/none/...
+        'view': 'month',  # or agendaWeek/basicWeek/agendaDay/basicDay
+        'filter': ['FILTER 1', 'FILTER 2', ...],
+        }),
+    ...
+    ]
+
+See also:
+`FullCalendar Documentation - Available Views
+<http://arshaw.com/fullcalendar/docs/views/Available_Views/>`_
+
+"""
+
 ORG_CAL_EVENT_CLASSIFIER = None
+"""
+A function to define extra eventclass.
+
+The following example defines 'home' event, depending on node tag::
+
+    def ORG_CAL_EVENT_CLASSIFIER(event):
+        if '@home' in event.tags:
+            return 'home'
+
+"""
+
 ORG_CAL_ADD_EVENTCLASSES = []
+"""
+Extra eventclasses to consider.
+
+Add eventclasses here if you defined `ORG_CAL_EVENT_CLASSIFIER`.
+
+Example::
+
+    ORG_CAL_ADD_EVENTCLASSES = ['home']
+
+"""
+
 ORG_CAL_ADD_EVENTSOURCES = []
+"""
+Extra event sources for FullCalendar.
+
+This is useful for adding events from another servers, such as
+holidays.  For example, to add Japanese holidays you can set it to::
+
+   [{'url': (
+        'https://www.google.com/calendar/feeds'
+        '/japanese%40holiday.calendar.google.com/public/basic'),
+     'color': '#AB8B00',
+     'textColor': '#AB8B00',
+     'backgroundColor': 'white',
+    }]
+
+- `FullCalendar Documentation - Google Calendar
+  <http://arshaw.com/fullcalendar/docs/google_calendar/>`_
+
+"""
 
 
 # ----------------------------------------------------------------------- #
